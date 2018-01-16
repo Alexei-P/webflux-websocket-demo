2a03ded5d3fe9fdd09d7d2bb6115689813cac2b7
Christian Davatz <crixx@davatz.eu>
2018-01-16 23:55:56 +0100

    Added remote websocket service
    Added continual message generation

--- src/main/java/org/springframework/webflux/websocket/webfluxwebsocketdemo/WebFluxWebSocketDemoApplication.java
+++ src/main/java/org/springframework/webflux/websocket/webfluxwebsocketdemo/WebFluxWebSocketDemoApplication.java
@@ -2,6 +2,8 @@ package org.springframework.webflux.websocket.webfluxwebsocketdemo;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Duration;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,12 +40,22 @@ public class WebFluxWebSocketDemoApplication {
 
 		EmitterProcessor<String> output = EmitterProcessor.create();
 
-		Mono<Void> sessionMono =
-				client.execute(new URI("ws://localhost:8080/echo"),
-						session -> session.receive()
-								.map(WebSocketMessage::getPayloadAsText)
-								.subscribeWith(output)
-								.then());
+		Mono<Void> sessionMono = client.execute(new URI("ws://localhost:8080/echo"),
+			session -> session.receive().map(WebSocketMessage::getPayloadAsText).subscribeWith(output).then());
+
+		return output.doOnSubscribe(s -> sessionMono.subscribe());
+	}
+
+	@GetMapping(path = "/remote", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> getRemoteStreaming() throws URISyntaxException {
+		Flux<String> input = Flux.<String>generate(sink -> sink.next(String.format("{got remote message: 'message', date: '%s' }", new Date())))
+			.delayElements(Duration.ofSeconds(1));
+
+		WebSocketClient client = new ReactorNettyWebSocketClient();
+		EmitterProcessor<String> output = EmitterProcessor.create();
+
+		Mono<Void> sessionMono = client.execute(URI.create("ws://echo.websocket.org"), session -> session.send(input.map(session::textMessage))
+			.thenMany(session.receive().map(WebSocketMessage::getPayloadAsText).subscribeWith(output).then()).then());
 
 		return output.doOnSubscribe(s -> sessionMono.subscribe());
 	}
@@ -52,10 +64,9 @@ public class WebFluxWebSocketDemoApplication {
 	public HandlerMapping webSocketMapping() {
 		Map<String, WebSocketHandler> map = new HashMap<>();
 
-		map.put("/echo", session ->
-				session.send(
-						Flux.just("foo", "bar", "baz", "cux")
-								.map(session::textMessage)));
+		map.put("/echo", session -> session.send(
+			Flux.<String>generate(sink -> sink.next(String.format("{ message: 'got local message', date: '%s' }", new Date())))
+				.delayElements(Duration.ofSeconds(1)).map(session::textMessage)));
 
 		SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
 		mapping.setUrlMap(map);
